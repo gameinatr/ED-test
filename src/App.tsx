2d7febae0002f544fef1034cd1d875b700851d90
Anton Malikau <anton.malikau@startmatter.com>
2023-09-26 23:23:58 +0300

    remove redundant layout and styling, add default cards and cards interface, render default cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,46 @@
 import "./App.css";
 
-import React from "react";
-import { Layout, Space } from "antd";
+import React, { useState } from "react";
+import { Card, Layout, Space } from "antd";
 const { Header, Footer, Sider, Content } = Layout;
 const headerStyle: React.CSSProperties = {
   textAlign: "center",
-  color: "#fff",
-  height: 64,
   paddingInline: 50,
   lineHeight: "64px",
-  backgroundColor: "#7DBCEA",
+  backgroundColor: "#fff",
 };
 const contentStyle: React.CSSProperties = {
   textAlign: "center",
-  minHeight: 120,
-  lineHeight: "120px",
-  color: "#fff",
-  backgroundColor: "#108EE9",
+  minHeight: "80vh",
 };
-const siderStyle: React.CSSProperties = {
-  textAlign: "center",
-  lineHeight: "120px",
-  color: "#fff",
-  backgroundColor: "#3BA0E9",
-};
-const footerStyle: React.CSSProperties = {
-  textAlign: "center",
-  color: "#fff",
-  backgroundColor: "#7DBCEA",
-};
-const App: React.FC = () => (
-  <Space direction="vertical" style={{ width: "100%" }} size={[0, 48]}>
-    <Layout>
-      <Header style={headerStyle}>Header</Header>
-      <Content style={contentStyle}>Content</Content>
-      <Footer style={footerStyle}>Footer</Footer>
-    </Layout>
-    <Layout>
-      <Header style={headerStyle}>Header</Header>
-      <Layout hasSider>
-        <Sider style={siderStyle}>Sider</Sider>
-        <Content style={contentStyle}>Content</Content>
-      </Layout>
-      <Footer style={footerStyle}>Footer</Footer>
-    </Layout>
-    <Layout>
-      <Header style={headerStyle}>Header</Header>
-      <Layout hasSider>
-        <Content style={contentStyle}>Content</Content>
-        <Sider style={siderStyle}>Sider</Sider>
-      </Layout>
-      <Footer style={footerStyle}>Footer</Footer>
-    </Layout>
-    <Layout>
-      <Sider style={siderStyle}>Sider</Sider>
+
+interface IToDoCard {
+  content: string;
+  date: number;
+  id: number;
+}
+
+const App: React.FC = () => {
+  const [cards, setCards] = useState<IToDoCard[]>([
+    { content: "Default ToDo 1", date: Date.now(), id: 1 },
+    { content: "Default ToDo 2", date: Date.now(), id: 2 },
+  ]);
+  return (
+    <Space direction="vertical" style={{ width: "100%" }} size={[0, 48]}>
       <Layout>
         <Header style={headerStyle}>Header</Header>
-        <Content style={contentStyle}>Content</Content>
-        <Footer style={footerStyle}>Footer</Footer>
+        <Content style={contentStyle}>
+          {cards.map((card: IToDoCard) => {
+            return (
+              <Card key={card.id} title={card.date.toString()}>
+                {card.content}
+              </Card>
+            );
+          })}
+        </Content>
       </Layout>
-    </Layout>
-  </Space>
-);
+    </Space>
+  );
+};
 
 export default App;
